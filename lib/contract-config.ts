b088836605d8f40a023ca1115481972b1fceba10
fukinlabs <fukinlabs@users.noreply.github.com>
2025-11-12 15:34:49 +0700

    update: change NFT contract address to 0xe81b2748149d089ebdae6fee36230d98ba00ff49

--- lib/contract-config.ts
+++ lib/contract-config.ts
@@ -6,5 +6,6 @@
  */
 
 // NFT Contract Address on Base
-export const NFT_CONTRACT_ADDRESS = "0xe716f7053E5C7188E48d0408bD601918f4Dc2d66" as const;
+//0xe81b2748149d089ebdae6fee36230d98ba00ff49, 0xe716f7053E5C7188E48d0408bD601918f4Dc2d66
+export const NFT_CONTRACT_ADDRESS = "0xe81b2748149d089ebdae6fee36230d98ba00ff49" as const;
 
