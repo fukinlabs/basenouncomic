14d81ee317dc855b31b902358f020a969be350e9
Giraroad <139453337+fukinlabs@users.noreply.github.com>
2025-11-18 22:57:18 +0700

    Update route.ts

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -1,5 +1,5 @@
 import { withValidManifest } from "@coinbase/onchainkit/minikit";
-import { minikitConfig } from "@/minikit.config";
+import { minikitConfig } from "../../minikit.config";
 
 export async function GET() {
   return Response.json(withValidManifest(minikitConfig));
